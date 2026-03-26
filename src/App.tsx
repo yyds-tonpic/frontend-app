48804821394794ef764de3b029c6e206b248f4ff
yyds <arduino.grand@gmail.com>
2026-03-26 15:08:31 +0700

    test error Code 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App) {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
